d8a4175cdcbb90070d959e74a749b052374cedc7
Francisco Emmanuel Dumas <91019174+Franz0204@users.noreply.github.com>
2022-05-11 18:46:08 +0800

    5/11/22
    >Made function battle function
    >Fixed the bug where the HP and EHP did not reset after retrying battles
    To do next:
    Design

--- MachineProject_2022.c
+++ MachineProject_2022.c
@@ -7,6 +7,20 @@ void Greetings()
 {
 	char cStart;
 	do {
+		printf("\t\t       __       _____                   _______            _            __\n");
+		printf("\t\t       \\ \\     |_   _|                 |__   __|          | |          / /\n");
+		printf("\t\t        \\ \\      | |  _ __ ___  _ __      | |_ __ __ _  __| | ___     / / \n");
+		printf("\t\t         \\ \\     | | | '__/ _ \\| '_ \\     | | '__/ _` |/ _` |/ _ \\   / /  \n");
+		printf("\t\t          \\ \\   _| |_| | | (_) | | | |    | | | | (_| | (_| |  __/  / /   \n");
+		printf("\t\t           \\_\\ |_____|_|  \\___/|_| |_|    |_|_|  \\__,_|\\__,_|\\___| /_/    \n\n");
+		printf("\t\t\t\t\t    ---TURN BASED---\n");
+		printf("\t------------------------------------------------------------------------------------------\n");	
+		printf("\t| After being handed the Iron Throne, King Bran of House Stark, decided to devise a      |\n");
+		printf("\t| strategic plan to improve the economic growth of the Six Kingdoms. The Royal Treasury  |\n");
+		printf("\t| currently has 2,000 Golden Dragons (GD) only. To accomplish this task, he instructed   |\n");
+		printf("\t| this Hand, Lord Tyrion Lannister, and his Master of Coin, Lord Bronn of the Blackwater |\n");
+		printf("\t| to go to trade with other kingdoms in Westeros and other free cities in Essos.         |\n");
+		printf("\t------------------------------------------------------------------------------------------\n\n");
 		printf("\t\t\t      |--------------Enter X to continue--------------|\t\t\t\n");
 		printf("\t\t\t\t\t           ");
 		scanf(" %c", &cStart);
@@ -67,8 +81,43 @@ void YourMove(int *nPhp, int *nPdmg, int nChoice, int *nSentinel){
 		}
 }
 
+void Battle (int *nDefeat, int *nSentinel, int *nPhp, int *nEhp, int *nPdmg, int *nEdmg){
+	
+	do {
+		srand(time(NULL));
+		int nChoice;
+	
+		printf("HP: %d \n", *nPhp);
+		printf("Enemy HP: %d \n", *nEhp);
+		
+		printf("Your Move \n>");
+		scanf("%d", &nChoice);
+				
+		YourMove(nPhp, nPdmg, nChoice, nSentinel);
+		EnemyMove(nEhp, nEdmg);
+					
+		*nPhp -= *nEdmg;
+		*nEhp -= *nPdmg;
+		*nEdmg = 0;
+		*nPdmg = 0;
+		system("cls");
+		
+		if (*nPhp <= 0) {
+			printf("Enemy Wins \n"); 
+			*nDefeat = 1;
+		}
+		else if (*nEhp <= 0)	{
+			printf("You Win \n"); 
+			*nDefeat = 1;
+		}
+		
+	} while (*nDefeat == 0 && *nSentinel == 0);
+	*nPhp = 100;
+	*nEhp = 100;
+}
+
 int main(){
-	int  nChoice, nSentinel = 0, nDefeat = 0, nRestart = 0;
+	int  nSentinel = 0, nDefeat = 0, nRestart = 0;
 	int nPhp = 100, nPdmg = 0, nEhp = 100, nEdmg = 0;
 	char cStart, cConfirm;
 
@@ -79,29 +128,7 @@ int main(){
 				nSentinel = 0;
 				srand(time(NULL));
 	
-				printf("HP: %d \n", nPhp);
-				printf("Enemy HP: %d \n", nEhp);
-				
-				printf("Your Move \n>");
-				scanf("%d", &nChoice);
-				
-				YourMove(&nPhp, &nPdmg, nChoice, &nSentinel);
-				EnemyMove(&nEhp, &nEdmg);
-			
-				
-				nPhp -= nEdmg;
-				nEhp -= nPdmg;
-				nEdmg = 0;
-				nPdmg = 0;
-				system("cls");
-				if (nPhp <= 0) {
-					printf("Enemy Wins"); 
-					nDefeat = 1;
-				}
-				else if (nEhp <= 0)	{
-					printf("You Win"); 
-					nDefeat = 1;
-				}
+			Battle(&nDefeat, &nSentinel, &nPhp, &nEhp, &nPdmg, &nEdmg);	
 				
 			} while (nDefeat == 0 && nSentinel == 0);
 
